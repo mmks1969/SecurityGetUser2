31378b04bf2ceef16133ceeff74e6fc2fc0e6f93
mitsushi.kohara <mitsushi.kohara@gr.tsubakimoto.co.jp>
2020-07-10 07:27:32 +0900

    new
    new

---
+++ src/main/java/com/example/GetUserApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetUserApplication.class, args);
+	}
+
+}
